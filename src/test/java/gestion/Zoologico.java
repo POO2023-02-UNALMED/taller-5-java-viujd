c569ed20cadbdc81731817b90e53594f6bd12e62
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-09-25 15:15:58 -0500

    Update Zoologico.java

--- src/test/java/gestion/Zoologico.java
+++ src/test/java/gestion/Zoologico.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Zoologico {
 	private String nombre;
 	private String ubicacion;
-	private ArrayList<Zona> zonas=new ArrayList<>();
+	private ArrayList<Zona> zonas=new ArrayList<Zona>();
 
 public Zoologico(String nombre, String ubicacion) {
 	this.nombre=nombre;
